582262542dd5e1decf5c51eff92d591331da6763
Srivarshine thoppe <165006714+Srivarshinets@users.noreply.github.com>
2025-07-26 11:29:19 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section id="home">
       <h1>Hello, I'm Srivarshine Thoppe</h1>
-      <p> Aspiring Frontend Developer | UI/UX Enthusiast</p>
+      <p> Cybersecurity Enthusiast | Passionate about AI/ML</p>
       <a href="/1NH22CE062_SRIVARSHINE[1].pdf" download>
         {" "}
       </a>
