85d749c3b5c9a6dfe19cae0a8979cc9da92230f2
Srivarshine thoppe <165006714+Srivarshinets@users.noreply.github.com>
2025-07-26 11:31:15 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,13 +5,12 @@ export default function Skills() {
     <section id="skills">
       <h2>Skills</h2>
       <ul>
-        <li>HTML, CSS, JavaScript</li>
-        <li>React js</li>
-        <li>Java</li>
         <li>C</li>
-        <li>SQL</li>
-        <li>Cloud Computing</li>
         <li>AI/ML</li>
+        <li>Java</li>
+        <li>Cybersecurity</li>
+        <li>HTML/CSS</li>
+        <li>Cloud Computing</li>
       </ul>
     </section>
   );
